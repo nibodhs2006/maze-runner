91e6ca862406c68c9c6e55d2576040fc72e0684d
nibodhs2006 <72917636+nibodhs2006@users.noreply.github.com>
2021-03-28 20:08:24 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# maze-runner
+# visual-snake-code README
+
+This game is an old school snake game that runs as a VS Code extension. Please read the requirements before using.
+
+## Features
+
+Press 'w' to go up, similarly 's' for down, 'a' for left, 'd' for right. At the end screen, you can press space to restart a game.
+
+### Screenshot 1:
+
+![gameplay1](screenshot/gameplay1.png)
+
+### Screenshot 2:
+
+![gameplay2](screenshot/gameplay2.png)
+
+## Requirements
+
+After installing the extension, press Ctrl(Command) + shift + p to open the shortcut for command. Type 'Visual Snake Code', then press enter the game will start.
+
+A suggestion is that you can disable editor.quickSuggestions to prevent the intellisense popping up every time when you press a key.
+
+## Extension Settings
+
+No settings.
+
+## Known Issues
+
+Sometimes the tail pixel doesn't function well, it drops. This bug is to be fixed.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of Visual Snake Code.
+
+### 1.0.1
+
+Add Change Log and Repository.
+
+### 1.0.2
+
+Minor fixes.
+
+### 1.0.3
+
+Fix a bug that tail drops sometimes.
+
+### 1.0.4
+
+Open new empty text document for the game. Thanks to @spoon611
+
+### 1.0.5
+
+Added score in the status bar.
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,389 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+var editor;
+var direction;
+var oldTail;
+var maxX;
+var minX;
+var maxY;
+var minY;
+var snake;
+var endGame;
+var line;
+var foodX;
+var foodY;
+var foodExist;
+var reset;
+var points = [
+	{
+		x: 10,
+		y: 31
+	}, {
+		x: 11,
+		y: 31
+	}, {
+		x: 12,
+		y: 31
+	}, {
+		x: 13,
+		y: 31
+	}, {
+		x: 14,
+		y: 31
+	}
+];
+var scoreStatusBarItem;
+var score;
+
+//21*30 area from [1,1] to [61, 61] 
+var startScreen = [
+	'╔══════════════════════════════════════════════════════════════╗\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                              ██                              ║\n',
+    '║                              ██                              ║\n',
+    '║                              ██                              ║\n',
+    '║                              ██                              ║\n',
+    '║                              ██                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '║                                                              ║\n',
+    '╚══════════════════════════════════════════════════════════════╝\n',
+	'w: up, s: down, a: left, d: right\n'
+];
+
+var endScreen = [
+	'╔══════════════════════════════════════════════════════════════╗',
+    '║                                                              ║',
+    '║                                                              ║',
+    '║                                                              ║',
+    '║            ████        ██      ██    ██   ██ ██ ██           ║',
+    '║          ██          ██  ██    ██ ██ ██   ██                 ║',
+    '║          ██    ██   ██    ██   ██    ██   ██ ██ ██           ║',
+    '║          ██    ██   ██ ██ ██   ██    ██   ██                 ║',
+    '║            ████     ██    ██   ██    ██   ██ ██ ██           ║',
+    '║                                                              ║',
+    '║                                                              ║',
+    '║                                                              ║',
+    '║            ████     ██    ██   ██ ██ ██   ██ ██ █            ║',
+    '║          ██    ██   ██    ██   ██         ██    ██           ║',
+    '║          ██    ██   ██    ██   ██ ██ ██   ██ ██ █            ║',
+    '║          ██    ██   ██    ██   ██         ██   ██            ║',
+    '║            ████        ██      ██ ██ ██   ██    ██           ║',                         
+    '║                                                              ║',
+    '║                                                              ║',
+    '║                   press space to restart                     ║',
+    '║                                                              ║',
+    '║                                                              ║',
+	'╚══════════════════════════════════════════════════════════════╝',
+	'w: up, s: down, a: left, d: right'
+];
+
+function gameInit(ed) {
+	clearScreen(ed, editor.document);
+	for(var i = 0; i < startScreen.length; i++) {
+		ed.insert(new vscode.Position(i, 0), startScreen[i]);
+	}
+	for(var x = 1; x <= 21; x++) {
+		snake[x] = [];
+		for(var y = 1; y <= 61; y = y + 2) {
+			snake[x][y] = 0;
+		}
+	}
+	for(let point of points) {
+		snake[point.x][point.y] = 1;
+	}
+}
+
+function keyInput(ed, doc) {
+	line = doc.lineCount - 1;
+	var command = doc.lineAt(line).text;
+	var i = command.length - 1;
+	if(direction != 'down' && command[i] == 'w') direction = 'up';
+	else if(direction != 'right' && command[i] == 'a') direction = 'left';
+	else if(direction != 'up' && command[i] == 's') direction = 'down';
+	else if(direction != 'left' && command[i] == 'd') direction = 'right';
+	else if(endGame && command[i] == ' ') reset = true;
+	//remove input char
+	ed.replace(
+		new vscode.Range(
+			new vscode.Position(line, 0),
+			new vscode.Position(line, 99999)
+		), ""
+	);
+}
+
+function createFood(ed) {
+	if(foodExist) return;
+	foodX = randomPos(1, 21);
+	foodY = randomPos(1, 61);
+	if(foodY % 2 == 0) foodY -= 1;
+	if(snake[foodX][foodY] == 1) {
+		createFood(ed);
+	} else {
+		ed.replace(
+			new vscode.Range(
+				new vscode.Position(foodX, foodY),
+				new vscode.Position(foodX, foodY + 2)
+			),'██'
+		);
+		foodExist = true;
+	}
+}
+
+function gameEngine() {
+	if(!endGame) {
+		editor.edit((ed) => {
+			keyInput(ed, editor.document);
+			createFood(ed);
+			playerMove();
+		})
+		.then(() => {
+			editor.edit((ed) => screenRender(ed));
+		})
+		.then(() => setTimeout(gameEngine, 200));
+	} else {
+		editor.edit((ed) => {
+			printEndScreen(ed);
+			keyInput(ed, editor.document);
+			resetGame();
+		 }).then(() => setTimeout(gameEngine, 200));
+	}
+}
+
+function screenRender(ed) {
+	ed.replace(
+		new vscode.Range(
+			new vscode.Position(points[0].x, points[0].y),
+			new vscode.Position(points[0].x, points[0].y + 2)
+		),'██'
+	);
+	snake[points[0].x][points[0].y] = 1;
+	
+	ed.replace(
+		new vscode.Range(
+			new vscode.Position(oldTail.x, oldTail.y),
+			new vscode.Position(oldTail.x, oldTail.y + 2)
+		),'  '
+	)
+	snake[oldTail.x][oldTail.y] = 0;
+}
+
+function eatFood(head) {
+	if(validatePos(head)) {
+		if(head.x == foodX && head.y == foodY) {
+			points.unshift(head);
+			foodExist = false;
+			updateScore();
+		} else {
+			points.unshift(head);
+			oldTail = points.pop();
+		}
+	}
+}
+
+function playerMove() {
+	if(direction == 'right') {
+		var head = {
+			x: points[0].x,
+			y: points[0].y + 2
+		}
+		eatFood(head);
+	} else if(direction == 'up') {
+		var head = {
+			x: points[0].x - 1,
+			y: points[0].y
+		}
+		eatFood(head);
+	} else if(direction == 'left') {
+		var head = {
+			x: points[0].x,
+			y: points[0].y - 2
+		}
+		eatFood(head);
+	} else if(direction == 'down') {
+		var head = {
+			x: points[0].x + 1,
+			y: points[0].y
+		}
+		eatFood(head);
+	}
+}
+
+function randomPos(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function validatePos(head) {
+	if(head.x < minX || head.x > maxX || head.y < minY || head.y > maxY || eatSelf(head)) {
+		endGame = true;
+		return false;
+	} 
+	return true;
+}
+
+function eatSelf(head) {
+	if(snake[head.x][head.y] == 1) return true;
+	return false;
+}
+
+function printEndScreen(ed) {
+	for(let i = 0; i < endScreen.length; i++) {
+		ed.replace(
+			new vscode.Range(
+				new vscode.Position(i, 0),
+				new vscode.Position(i, 64)
+			), endScreen[i]
+		);
+	}
+}
+
+function resetGame() {
+	if(reset) {
+		reset = false;
+		endGame = false;
+		foodExist = false;
+		direction = 'up';
+		snake = [];
+		points = [
+			{
+				x: 10,
+				y: 31
+			}, {
+				x: 11,
+				y: 31
+			}, {
+				x: 12,
+				y: 31
+			}, {
+				x: 13,
+				y: 31
+			}, {
+				x: 14,
+				y: 31
+			}
+		];
+		editor.edit((ed) => {
+			clearScreen(ed, editor.document);
+		}).then(() => {
+			editor.edit((ed) => {
+				gameInit(ed);
+			});
+		});
+	}
+}
+
+function clearScreen(ed, doc) {
+	resetScore();
+	var last = doc.lineCount - 1;
+	ed.delete(
+		new vscode.Range(
+			new vscode.Position(0, 0),
+			new vscode.Position(last, 99999)
+		)
+	);
+}
+
+function updateScore() {
+	score += 1;	
+	updateScoreStatusBarItem();
+}
+
+function resetScore() {
+	score = 0;
+	updateScoreStatusBarItem()
+}
+
+function updateScoreStatusBarItem() {
+	if(scoreStatusBarItem) {
+		scoreStatusBarItem.text = "Score: " + score;
+	}
+}
+
+function initializeScoreStatusBarItem() {
+	scoreStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	scoreStatusBarItem.tooltip = "Current game score";
+	scoreStatusBarItem.show();
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "visual-snake-code" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.vSnakeCode', function () {
+		// The code you place here will be executed every time your command is executed
+		minX = 1;
+		minY = 1;
+		maxX = 21;
+		maxY = 61;
+		reset = false;
+		endGame = false;
+		foodExist = false;
+		direction = 'up';
+		snake = [];
+		points = [
+			{
+				x: 10,
+				y: 31
+			}, {
+				x: 11,
+				y: 31
+			}, {
+				x: 12,
+				y: 31
+			}, {
+				x: 13,
+				y: 31
+			}, {
+				x: 14,
+				y: 31
+			}
+		];
+
+		vscode.workspace.openTextDocument().then(newDocument => {
+			vscode.window.showTextDocument(newDocument).then(newTextEditor => {
+				editor = newTextEditor;
+				editor.edit((e) => gameInit(e)).then(gameEngine);
+			});
+		});
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Visual Snake Code!');
+	});
+
+	initializeScoreStatusBarItem();
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
